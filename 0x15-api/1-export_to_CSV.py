3f1c4a636b3865a3d9c4cbf6541de8d94e472c01
DjibbeDiguina <diguinafils1@gmail.com>
2024-04-22 14:21:32 -0700

    api good

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -14,21 +14,21 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
 
     # Fetch user information from the API and
-    #   convert the response to a JSON object
+    # convert the response to a JSON object
     user = requests.get(url + "users/{}".format(user_id)).json()
 
     # Extract the username from the user data
     username = user.get("username")
 
     # Fetch the to-do list items associated with the
-    #   given user ID and convert the response to a JSON object
+    # given user ID and convert the response to a JSON object
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
     # Use list comprehension to iterate over the to-do list items
     # Write each item's details (user ID, username, completion status,
-    #   and title) as a row in the CSV file
+    # and title) as a row in the CSV file
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow(
- [user_id, username, t.get("completed"), t.get("title")]
+            [user_id, username, t.get("completed"), t.get("title")]
          ) for t in todos]
